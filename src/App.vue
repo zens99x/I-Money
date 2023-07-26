114eb559a79f6ce01dfde7dd5cae0c12fd7f9f90
Thanh Nam <thanhnam0299x@gmail.com>
2023-07-26 09:23:57 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { fireStoreCore } from "./configs/firebase.js";
+import { getDocs, collection } from "firebase/firestore";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    setup() {
+        // const fetchData = async () => {
+        //     try {
+        //         const querySnapshot = await getDocs(
+        //             collection(fireStoreCore, "transactions")
+        //         );
+        //         querySnapshot.forEach((doc) => {
+        //             console.log(doc.id, " => ", doc.data());
+        //         });
+        //     } catch (error) {
+        //         console.error("Error getting documents:", error);
+        //     }
+        // };
+
+        // fetchData();
+
+        async function getTransactions() {
+            const res = await getDocs(
+                collection(fireStoreCore, "transactions")
+            );
+            const data = res.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id };
+            });
+            console.log(data);
+        }
+        getTransactions();
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
